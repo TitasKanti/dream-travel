af3c20d955296f372d70b8bbf04771ca4161dfb2
TitasKanti <adv.titas.bd@gmail.com>
2021-10-29 22:33:56 +0600

    header and footer update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,81 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer.</h2>
+        <div className="py-8 w-full mt-20 bg-gray-700">
+            <div className="container mx-auto px-8">
+                <div className="grid md:grid-cols-5 no-underline w-full text-white">
+                    <div className="h-10 w-10 self-center mr-2 flex flex-row">
+                        {/* <img src={icon} alt="icon" /> */}
+
+                        <Link to="/home" className="md:text-3xl no-underline text-grey-darkest hover:text-blue-200 font-sans font-bold">Dream Travel</Link><br />
+                    </div>
+
+                    <div className="block sm:table-cell">
+                        <p className="uppercase text-grey text-sm sm:mb-6">Links</p>
+                        <ul className="list-reset no-underline text-xs mb-6">
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">FAQ</Link>
+                            </li>
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">Help</Link>
+                            </li>
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">Support</Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="block sm:table-cell">
+                        <p className="uppercase text-grey text-sm sm:mb-6">Legal</p>
+                        <ul className="list-reset text-xs mb-6">
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">Terms</Link>
+                            </li>
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">Privacy</Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="block sm:table-cell">
+                        <p className="uppercase text-grey text-sm sm:mb-6">Social</p>
+                        <ul className="list-reset text-xs mb-6">
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">Facebook</Link>
+                            </li>
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">Linkedin</Link>
+                            </li>
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">Twitter</Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="block sm:table-cell">
+                        <p className="uppercase text-grey text-sm sm:mb-6">Company</p>
+                        <ul className="list-reset text-xs mb-6">
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">Official Blog</Link>
+                            </li>
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">About Us</Link>
+                            </li>
+                            <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
+                                <Link to="#" className="text-grey hover:text-blue-200">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="text-center pb-2 text-white">
+                <small><i><a href="http://www.unsplash.com">Logos and images sourced from Freepik</a></i></small> <br />
+                <small>© 2021 TK-tech-zone/DreamTravel</small>
+            </div>
         </div>
+
     );
 };
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is Header.</h2>
+            <nav className="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
+
+                <div className="mb-2 sm:mb-0 flex flex-row">
+                    <div className="h-10 w-10 self-center mr-2">
+                        {/* <img src={icon} alt="icon" /> */}
+                    </div>
+                    <div>
+                        <NavLink to="/home" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">Dream Travel</NavLink><br />
+                    </div>
+                </div>
+
+                <div className="sm:mb-0 self-center">
+
+                    <NavLink to="/home" activeStyle={{ fontWeight: "bold", color: "blue" }} className="text-md no-underline text-black ml-3 px-1">Home</NavLink>
+
+                    <NavLink to="/about" activeStyle={{ fontWeight: "bold", color: "blue" }} className="text-md no-underline text-grey-darker ml-3 px-1">About</NavLink>
+
+                    {/* <NavLink to="/events" activeStyle={{ fontWeight: "bold", color: "blue" }} className="text-lg no-underline text-grey-darkest ml-3">Events</NavLink>
+
+                    <NavLink to="/allServices" activeStyle={{ fontWeight: "bold", color: "blue" }} className="text-md no-underline text-grey-darker ml-3 px-1">Services</NavLink> */}
+
+                    {/* <NavLink to="/reviews" activeStyle={{ fontWeight: "bold", color: "blue" }} className="text-md no-underline text-grey-darker ml-3 px-1">Reviews</NavLink>
+                    <span className="text-blue-700 mx-3"> {user.displayName} </span> */}
+                    {/* {
+                        user.email ?
+                            <button className="text-red-700" onClick={logOut}>Sign-Out</button>
+                            :
+                            <NavLink to="/signin" activeStyle={{ fontWeight: "bold", color: "blue" }} className="text-md no-underline text-grey-darker ml-3 px-1">Sign-In</NavLink>
+                    } */}
+
+
+                </div>
+            </nav>
         </div>
     );
 };
