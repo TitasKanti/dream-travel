71db42fa92e0dadba5c987b0833fa6c89f8a9785
TitasKanti <adv.titas.bd@gmail.com>
2021-10-31 23:17:05 +0600

    some update

--- src/Components/AddNewEvent/AddNewEvent.js
+++ src/Components/AddNewEvent/AddNewEvent.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import './AddNewEvent.css';
 
 const AddNewEvent = () => {
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+        const newEvent = { data }
+        fetch('http://localhost:5000/addEvent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newEvent)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert('successfully added the user.')
+                }
+            })
+        reset();
+    }
+
     return (
-        <div>
-            <h2> Add a New Event</h2>
-        </div>
+        <div className="add-service">
+            <h2 className="m-4">Please Add a Service</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name", { required: true, maxLength: 20 })} placeholder="name" />
+                <textarea {...register("duration")} placeholder="description" />
+                <input type="text" {...register("price")} placeholder="price" />
+                <input  {...register("img")} placeholder="image" />
+                <input type="submit" />
+            </form>
+        </div >
     );
 };
 
